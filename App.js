95d70af17546b8b50797238582355c4da2d0a234
Apple <roto93@yahoo.com.tw>
2021-04-09 23:05:42 +0800

    Add branch Hw4

--- App.js
+++ App.js
@@ -1,32 +1,35 @@
-//Hw 8
+//Hw4 components
 
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
-import PokeListScreen from './src/screen/PokeListScreen'
-import MyPokemonScreen from './src/screen/MyPokemonScreen'
-import store from './src/redux/store'
-import { StoreContext, useMappedState, useDispatch } from 'redux-react-hook'
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import MyGoodBtn from './src/component/MyGoodBtn'
 
-function Hw8() {
-    const Stack = createStackNavigator()
+function hw4(props) {
+
     return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name={'PokeList'} component={PokeListScreen} />
-                <Stack.Screen name={'MyPokemon'} component={MyPokemonScreen} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
+        <View style={styles.container}>
+            <Text style={styles.title}>自製按鈕</Text>
+            <MyGoodBtn t={'NO.1'} tSize={12} isBorderRadius={true} />
+            <MyGoodBtn t={'NO.2'} tSize={20} isBorderRadius={false} />
+            <MyGoodBtn t={'NO.3'} tSize={16} isBorderRadius={true} />
+            <MyGoodBtn />
+        </View>
+    );
 }
 
-const styles = StyleSheet.create({})
+export default hw4;
 
-export default function App() {
-    return (
-        <StoreContext.Provider value={store()}>
-            <Hw8 />
-        </StoreContext.Provider>
-    )
-}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        justifyContent: 'center',
+        alignItems: 'center',
+
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        color: '#4B88A2'
+    }
+})
