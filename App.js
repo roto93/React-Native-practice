2b3638c4c9f9dc3558bd5810136ec1649c9004a1
Apple <roto93@yahoo.com.tw>
2021-04-09 23:02:48 +0800

    Add branch Hw5

--- App.js
+++ App.js
@@ -1,32 +1,72 @@
-//Hw 8
+// hw5 Navigation
 
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
-import PokeListScreen from './src/screen/PokeListScreen'
-import MyPokemonScreen from './src/screen/MyPokemonScreen'
-import store from './src/redux/store'
-import { StoreContext, useMappedState, useDispatch } from 'redux-react-hook'
-
-function Hw8() {
-    const Stack = createStackNavigator()
-    return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name={'PokeList'} component={PokeListScreen} />
-                <Stack.Screen name={'MyPokemon'} component={MyPokemonScreen} />
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
-}
-
-const styles = StyleSheet.create({})
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, View, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import BirthdayPage from './src/screen/BirthdayPage'
+import HoroscopePage from './src/screen/HoroscopePage'
+import SettingPage from './src/screen/SettingPage'
+import AnswerPage from './src/screen/AnswerPage'
+import Ionicons from 'react-native-vector-icons/Ionicons'
 
 export default function App() {
+
+    const Tab = createBottomTabNavigator()
+    const Stack = createStackNavigator()
+
+    const BirthdayStackPage = () => {
+        return (
+            <Stack.Navigator>
+                <Stack.Screen component={BirthdayPage} name={'Birthday'}
+                    options={{ title: '星座判斷器', headerTitleAlign: 'center' }}
+                />
+                <Stack.Screen component={HoroscopePage} name={'Horoscope'}
+                    options={{ title: '星座判斷器', headerTitleAlign: 'center' }}
+                />
+            </Stack.Navigator>
+        )
+    }
+
+    const ChoicePage = () => {
+        return (
+            <Stack.Navigator>
+                <Stack.Screen component={SettingPage} name={'Setting'}
+                    options={{ title: '選擇障礙小幫手', headerTitleAlign: 'center' }} />
+                <Stack.Screen component={AnswerPage} name={'Answer'}
+                    options={{ title: 'Here\'s your 選擇', headerTitleAlign: 'center' }} />
+            </Stack.Navigator>
+        )
+    }
+
+
     return (
-        <StoreContext.Provider value={store()}>
-            <Hw8 />
-        </StoreContext.Provider>
-    )
+        <NavigationContainer>
+            <Tab.Navigator tabBarOptions={{ labelStyle: { fontSize: 12 } }}>
+                <Tab.Screen component={BirthdayStackPage} name={'BirthdayStack'}
+                    options={{
+                        tabBarIcon: ({ color }) => (<Ionicons name={'md-planet'} size={30} color={color} />),
+                        tabBarLabel: '星座',
+
+                    }}
+                />
+                <Tab.Screen component={ChoicePage} name={'Choice'}
+                    options={{
+                        tabBarIcon: ({ color }) => (<Ionicons name={'md-infinite'} size={30} color={color} />),
+                        tabBarLabel: '選擇障礙',
+                    }}
+                />
+            </Tab.Navigator>
+        </NavigationContainer>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#FFE9F3',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+})
--- src/redux/reducer/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { combineReducers } from 'redux'
-import showPokeModalReducer from './showPokeModalReducer'
-import idSavedReducer from './idSavedReducer'
-
-export const rootReducer = combineReducers({
-    showPokeModalReducer,
-    idSavedReducer,
-})
