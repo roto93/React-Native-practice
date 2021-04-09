b85ee518c65b4e402847bb9fb7d571aecc75d829
Apple <roto93@yahoo.com.tw>
2021-04-09 23:19:52 +0800

    reset main branch

--- App.js
+++ App.js
@@ -2,31 +2,19 @@
 
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
-import PokeListScreen from './src/screen/PokeListScreen'
-import MyPokemonScreen from './src/screen/MyPokemonScreen'
-import store from './src/redux/store'
-import { StoreContext, useMappedState, useDispatch } from 'redux-react-hook'
 
-function Hw8() {
-    const Stack = createStackNavigator()
+export default function Hw8() {
     return (
-        <NavigationContainer>
-            <Stack.Navigator>
-                <Stack.Screen name={'PokeList'} component={PokeListScreen} />
-                <Stack.Screen name={'MyPokemon'} component={MyPokemonScreen} />
-            </Stack.Navigator>
-        </NavigationContainer>
+        <View style={styles.container}>
+            <Text>Hi React Native</Text>
+        </View>
     )
 }
 
-const styles = StyleSheet.create({})
-
-export default function App() {
-    return (
-        <StoreContext.Provider value={store()}>
-            <Hw8 />
-        </StoreContext.Provider>
-    )
-}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
