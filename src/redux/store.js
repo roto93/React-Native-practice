371d57ad2ea22bbe93f233ed843c836f8b83810d
Apple <roto93@yahoo.com.tw>
2021-04-09 18:25:33 +0800

    Add branch Hw7

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore } from 'redux'
-import { rootReducer } from '../redux/reducer/index'
-
-export default function configureStore() {     //建立一個函數給App使用
-    let store = createStore(rootReducer)      //用createStare()建立一個Redux-store，裡面放一個reducer: rootReducer
-    //console.log(store.getState())
-    return store
-}
